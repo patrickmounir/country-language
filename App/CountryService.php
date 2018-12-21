bcf6b917d033715a057060324713c3de5d28b32a
Patrick Attia <patrickmounir@gmail.com>
2018-12-21 15:46:50 +0200

    [Implementation] Added implementation and success case

--- App/CountryService.php
+++ App/CountryService.php
@@ -26,8 +26,20 @@ class CountryService
         return $response[0]['languages'][0]['iso639_1'];
     }
 
-    public function getCountriesWithLanguage()
+    public function getCountriesWithLanguage(string $languageCode)
     {
-        
+        $curl = curl_init("https://restcountries.eu/rest/v2/lang/{$languageCode}?fields=name");
+
+        $options = [
+            CURLOPT_RETURNTRANSFER => true
+        ];
+
+        curl_setopt_array($curl, $options);
+
+        $response = json_decode(curl_exec($curl), true);
+
+        curl_close($curl);
+
+        return collect($response)->pluck('name')->toArray();
     }
 }
--- Tests/GetCountriesWithSameLanguageTest.php
+++ Tests/GetCountriesWithSameLanguageTest.php
@@ -7,6 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 class GetCountriesWithSameLanguageTest extends TestCase
 {
+    private function countriesThatSpeakSpanish()
+    {
+       $countries = json_decode('[{"name":"Argentina"},{"name":"Belize"},{"name":"Bolivia (Plurinational State of)"},{"name":"Chile"},{"name":"Colombia"},{"name":"Costa Rica"},{"name":"Cuba"},{"name":"Dominican Republic"},{"name":"Ecuador"},{"name":"El Salvador"},{"name":"Equatorial Guinea"},{"name":"Guam"},{"name":"Guatemala"},{"name":"Honduras"},{"name":"Mexico"},{"name":"Nicaragua"},{"name":"Panama"},{"name":"Paraguay"},{"name":"Peru"},{"name":"Puerto Rico"},{"name":"Spain"},{"name":"Uruguay"},{"name":"Venezuela (Bolivarian Republic of)"},{"name":"Western Sahara"}]', true);
+
+        return collect($countries)->pluck('name')->toArray();
+    }
     /** @test */
     function it_has_method_getCountriesWithLanguage()
     {
@@ -14,6 +20,19 @@ class GetCountriesWithSameLanguageTest extends TestCase
 
         $this->assertTrue(
             method_exists($countryService, 'getCountriesWithLanguage'),
+            'Class does not have method getCountriesWithLanguage'
+        );
+    }
+
+    /** @test */
+    function it_returns_the_countries_that_speak_spanish()
+    {
+        $countryService = new CountryService();
+
+
+        $this->assertEquals(
+            $this->countriesThatSpeakSpanish(),
+            $countryService->getCountriesWithLanguage('es'),
             'Class does not have method getCountriesWithLanguage'
         );
     }
