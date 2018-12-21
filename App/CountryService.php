d772f2b315f295bdcd62a1123a4e3e1c6856eaae
Patrick Attia <patrickmounir@gmail.com>
2018-12-21 15:13:13 +0200

    [Correct Input] returns the language for the correct input

--- App/CountryService.php
+++ App/CountryService.php
@@ -5,8 +5,20 @@ namespace App;
 class CountryService
 {
 
-    public function getCountryLanguage()
+    public function getCountryLanguage(string $country)
     {
+        $curl = curl_init("https://restcountries.eu/rest/v2/name/{$country}?fullText=true");
+
+        $options = [
+            CURLOPT_RETURNTRANSFER => true
+        ];
         
+        curl_setopt_array($curl, $options);
+
+        $response = json_decode(curl_exec($curl), true);
+
+        curl_close($curl);
+
+        return $response[0]['languages'][0]['iso639_1'];
     }
 }
--- Tests/GetCountryLanguageTest.php
+++ Tests/GetCountryLanguageTest.php
@@ -17,4 +17,16 @@ class GetCountryLanguageTest extends TestCase
             'Class does not have method getCountryLanguage'
         );
     }
+
+    /** @test */
+    function it_returns_es_for_Spain()
+    {
+        $countryService = new CountryService();
+
+        $this->assertEquals(
+            'es',
+            $countryService->getCountryLanguage('Spain'),
+            'Returned the wrong output for the Country Spain'
+        );
+    }
 }
